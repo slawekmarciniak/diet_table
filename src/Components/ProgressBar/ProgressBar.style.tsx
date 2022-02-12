10c7320ddbc6b7338ab7ee0ee27d8da74eb4edce
slawekmarciniak <slawek.jazz@gmail.com>
2022-02-12 11:32:16 +0100

    add dots colors in progress bar

--- src/Components/ProgressBar/ProgressBar.style.tsx
+++ src/Components/ProgressBar/ProgressBar.style.tsx
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 
+interface Props {
+    bcg: string;
+}
+
 export const BarContainer = styled.div`
     display: flex;
 `;
 
-export const Dot = styled.div`
-    width: 5px;
-    height: 5px;
+export const Dot = styled.div<Props>`
+    width: 8px;
+    height: 8px;
     border-radius: 50%;
-    background-color: red;
+    background-color: ${(props) => props.bcg};
+    border: 1px solid gray;
 `;
--- src/Components/ProgressBar/ProgressBar.tsx
+++ src/Components/ProgressBar/ProgressBar.tsx
@@ -5,12 +5,17 @@ import { userCurrentWeek, userWeeks } from "../../fakeDB/fakeDB";
 interface ProgressBarProps {}
 
 const ProgressBar: FC<ProgressBarProps> = () => {
+    const renderDot = (e: number, i: number) => {
+        let background = "gray";
+        if (e === userCurrentWeek) {
+            background = "white";
+        } else if (e < userCurrentWeek) {
+            background = "green";
+        }
+        return <Dot key={i} bcg={background} />;
+    };
     return (
-        <BarContainer>
-            {userWeeks.map(() => (
-                <Dot />
-            ))}
-        </BarContainer>
+        <BarContainer>{userWeeks.map((e, i) => renderDot(e, i))}</BarContainer>
     );
 };
 
