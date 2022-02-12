3632fdd9065007a5895d72b118b6c4ab9a66803b
slawekmarciniak <slawek.jazz@gmail.com>
2022-02-12 22:26:07 +0100

    add pseudoelements to progress bar, as number containers

--- src/Components/ProgressBar/ProgressBar.style.tsx
+++ src/Components/ProgressBar/ProgressBar.style.tsx
@@ -2,16 +2,41 @@ import styled from "styled-components";
 
 interface Props {
     bcg: string;
+    digit: string;
+}
+interface DashProps {
+    border: string;
 }
 
 export const BarContainer = styled.div`
     display: flex;
+    align-items: center;
 `;
 
 export const Dot = styled.div<Props>`
-    width: 8px;
-    height: 8px;
+    width: 9px;
+    height: 9px;
     border-radius: 50%;
     background-color: ${(props) => props.bcg};
     border: 1px solid gray;
+    position: relative;
+    &:after {
+        content: "${(props) => props.digit}";
+        position: absolute;
+        width: 8px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        top: 14px;
+        left: -1px;
+        color: gray;
+        z-index: 2;
+        font-size: 10px;
+    }
+`;
+
+export const Dash = styled.div<DashProps>`
+    border: ${(props) => props.border};
+    height: 2px;
+    width: 8px;
 `;
--- src/Components/ProgressBar/ProgressBar.tsx
+++ src/Components/ProgressBar/ProgressBar.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
-import { Dot, BarContainer } from "./ProgressBar.style";
+import React, { FC } from "react";
+import { Dot, BarContainer, Dash } from "./ProgressBar.style";
 import { userCurrentWeek, userWeeks } from "../../fakeDB/fakeDB";
 
 interface ProgressBarProps {}
@@ -7,12 +7,23 @@ interface ProgressBarProps {}
 const ProgressBar: FC<ProgressBarProps> = () => {
     const renderDot = (e: number, i: number) => {
         let background = "gray";
+        let border = "1px solid gray";
         if (e === userCurrentWeek) {
             background = "white";
         } else if (e < userCurrentWeek) {
             background = "green";
         }
-        return <Dot key={i} bcg={background} />;
+        if (e < userCurrentWeek - 1) {
+            border = "1px solid green";
+        }
+        return (
+            <React.Fragment key={e}>
+                <Dot key={`${e}_dot`} bcg={background} digit={e.toString()} />
+                {e < userWeeks.length && (
+                    <Dash key={`${e}_dash`} border={border} />
+                )}
+            </React.Fragment>
+        );
     };
     return (
         <BarContainer>{userWeeks.map((e, i) => renderDot(e, i))}</BarContainer>
